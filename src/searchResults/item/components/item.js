0a3989d9cd84328218a23cbd7007b84f722bac87
Gilmar Vanin <jvanin@thoughtworks.com>
2018-09-28 18:33:26 -0300

    Uses font awesome icons.

--- src/searchResults/item/components/item.js
+++ src/searchResults/item/components/item.js
@@ -1,4 +1,7 @@
 import React from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faStar as StarIcon, faSmile as SmileIcon } from "@fortawesome/free-regular-svg-icons"
+import { faLocationArrow as LocationArrowIcon } from "@fortawesome/free-solid-svg-icons"
 import "../item.css"
 
 class Item extends React.Component {
@@ -8,8 +11,21 @@ class Item extends React.Component {
 				<div className="thumbnail"></div>
 				<div className="general-details">
 					<h2>Quentin Amsterdam Hotel</h2>
-					<span className="location"> 120 m to city center - Jordan </span>
-					<span className="rating"> Amazing 9.2 </span>
+					<div className="stars">
+						<FontAwesomeIcon icon={StarIcon} color="#FF7C44"/>
+						<FontAwesomeIcon icon={StarIcon} color="#FF7C44"/>
+						<FontAwesomeIcon icon={StarIcon} color="#FF7C44"/>
+						<FontAwesomeIcon icon={StarIcon} color="#e5e5e5"/>
+						<FontAwesomeIcon icon={StarIcon} color="#e5e5e5"/>
+					</div>
+					<div className="location">
+						<FontAwesomeIcon icon={LocationArrowIcon} color="#0098E8"/>
+						<span> 120 m to city center - Jordan </span>
+					</div>
+					<div className="rating">
+						<FontAwesomeIcon icon={SmileIcon} color="#82BB7F"/>
+						<span> Amazing 9.2 </span>
+					</div>
 					<ul className="tags">
 						<li> Amazing SPA </li>
 						<li> Good food </li>
