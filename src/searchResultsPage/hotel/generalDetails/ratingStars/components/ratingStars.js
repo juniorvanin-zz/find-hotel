2df3420899d546bf9316a1d1bfaf89a7b6637122
Gilmar Vanin <jvanin@thoughtworks.com>
2018-10-01 23:09:47 -0300

    Uses ramda times to avoid imutability.

--- src/searchResultsPage/hotel/generalDetails/ratingStars/components/ratingStars.js
+++ src/searchResultsPage/hotel/generalDetails/ratingStars/components/ratingStars.js
@@ -1,26 +1,22 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { times } from "ramda"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faStar as StarIcon } from "@fortawesome/free-regular-svg-icons"
 
 const ORANGE = "#FF7C44"
 const GRAY = "#e0dede"
 
-const getStarColorByRate = (index, numberOfStars) => numberOfStars >= index ? ORANGE : GRAY
+const getStarColorByRate = (index, numberOfStars) => numberOfStars > index ? ORANGE : GRAY
 
-const RatingStars = (props) => {
-	const stars = []
-
-	for(let i = 1; i <= 5; i++) {
-		stars.push(<FontAwesomeIcon  key={i} icon={StarIcon} color={ getStarColorByRate(i, props.numberOfStars) }/>)
-	}
-
-	return (
-		<div className="stars">
-			{ stars }
-		</div>
-	)
-}
+const RatingStars = (props) => (
+	<div className="stars">
+		{
+			times((index) => <FontAwesomeIcon key={index}
+				icon={StarIcon} color={ getStarColorByRate(index, props.numberOfStars) } />, 5)
+		}
+	</div>
+)
 
 RatingStars.propTypes = {
 	numberOfStars: PropTypes.number
