e71020e55a2684e071b0b0e3c192adf691f7ae0f
Gilmar Vanin <jvanin@thoughtworks.com>
2018-10-02 21:20:55 -0300

    Adds filters hotel results for mobile version.

--- src/searchResultsPage/filters/components/filters.js
+++ src/searchResultsPage/filters/components/filters.js
@@ -1,10 +1,15 @@
 import React, { Fragment } from "react"
 import PropTypes from "prop-types"
+import MediaQuery from "react-responsive"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faFilter as FilterIcon, faSortAmountDown as SortIcon } from "@fortawesome/free-solid-svg-icons"
 
 import DistanceInput from "searchResultsPage/filters/distanceInput/components/distanceInput"
 import MinRatingInput from "searchResultsPage/filters/minRatingInput/components/minRatingInput"
 import MaxPriceInput from "searchResultsPage/filters/maxPriceInput/components/maxPriceInput"
 import SortBy from "searchResultsPage/filters/sortBy/components/sortBy"
+
+const BLUE = "#0092e5"
 
 class Filters extends React.Component {
 	constructor(props) {
@@ -15,34 +20,54 @@ class Filters extends React.Component {
 		const options = this.props.options
 		return (
 			<Fragment>
-				<div className="input-range-group">
-					<div className="input-range-wrapper">
-						<MaxPriceInput
-							minValue={ options.priceRange.min }
-							maxValue={ options.priceRange.max }
-							selectedValue={ options.priceRange.selectedValue }
-						/>
+				<MediaQuery minWidth={800}>
+					<div className="input-range-group">
+						<div className="input-range-wrapper">
+							<MaxPriceInput
+								minValue={ options.priceRange.min }
+								maxValue={ options.priceRange.max }
+								selectedValue={ options.priceRange.selectedValue }
+							/>
+						</div>
+						<div className="input-range-wrapper">
+							<MinRatingInput
+								minValue={ options.ratingRange.min }
+								maxValue={ options.ratingRange.max }
+								selectedValue={ options.ratingRange.selectedValue }
+							/>
+						</div>
+						<div className="input-range-wrapper">
+							<DistanceInput
+								minValue={ options.distanceRange.min }
+								maxValue={ options.distanceRange.max }
+								selectedValue={ options.distanceRange.selectedValue }
+							/>
+						</div>
 					</div>
-					<div className="input-range-wrapper">
-						<MinRatingInput
-							minValue={ options.ratingRange.min }
-							maxValue={ options.ratingRange.max }
-							selectedValue={ options.ratingRange.selectedValue }
-						/>
-					</div>
-					<div className="input-range-wrapper">
-						<DistanceInput
-							minValue={ options.distanceRange.min }
-							maxValue={ options.distanceRange.max }
-							selectedValue={ options.distanceRange.selectedValue }
-						/>
-					</div>
-				</div>
-				<SortBy { ...this.props.sortByInfo } />
+					<SortBy { ...this.props.sortByInfo } />
+				</MediaQuery>
+				<MediaQuery maxWidth={800}>
+					<SortByMobileButton />
+					<FilterByMobileButton />
+				</MediaQuery>
 			</Fragment>
 		)
 	}
 }
+
+const SortByMobileButton = () => (
+	<button className="mobile-filter-button">
+		<FontAwesomeIcon icon={ SortIcon } color={BLUE} />
+		<span> Sort by </span>
+	</button>
+)
+
+const FilterByMobileButton = () => (
+	<button className="mobile-filter-button">
+		<FontAwesomeIcon icon={ FilterIcon } color={BLUE} />
+		<span> Filter by </span>
+	</button>
+)
 
 Filters.propTypes = {
 	sortByInfo: PropTypes.object,
