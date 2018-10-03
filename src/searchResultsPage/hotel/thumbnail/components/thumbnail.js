3242562b86995ded5aaba58e6b1f8f268198eb1a
Gilmar Vanin <jvanin@thoughtworks.com>
2018-10-03 01:18:08 -0300

    Adds favorite hotel icon on thumbnail.

--- src/searchResultsPage/hotel/thumbnail/components/thumbnail.js
+++ src/searchResultsPage/hotel/thumbnail/components/thumbnail.js
@@ -1,5 +1,8 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faHeart as HeartIcon } from "@fortawesome/free-regular-svg-icons"
+import { faHeart as FullHeartIcon } from "@fortawesome/free-solid-svg-icons"
 
 import "searchResultsPage/hotel/thumbnail/thumbnail.less"
 
@@ -12,6 +15,12 @@ const Thumbnail = (props) => {
 				<div className="arrow-right"></div>
 			</div>
 			<img src={ props.url }/>
+			<div className="favorite">
+				<span className="fa-layers fa-fw">
+					<FontAwesomeIcon icon={FullHeartIcon} className="half-opacity" color="black" size="2x"/>
+					<FontAwesomeIcon icon={HeartIcon}  color="white" size="2x"/>
+				</span>
+			</div>
 		</div>
 	)
 }
